564d41f114ea0ae9a8016c9c791efe7a1a83649e
Brandon Kipkorir Boit <bkb57@en-ci-cisugcl20..coecis.cornell.edu>
2025-09-22 19:16:32 -0400

    implemented part2

--- part2/knn.hpp
+++ part2/knn.hpp
@@ -98,7 +98,56 @@ Node<T>* buildKD(std::vector<std::pair<T,int>>& items, int depth = 0)
     You should recursively construct the tree and return the root node.
     For now, this is a stub that returns nullptr.
     */
-    return nullptr;
+    if (items.empty()) {
+        return nullptr;
+    }
+    
+    if (items.size() == 1) {
+        Node<T>* node = new Node<T>();
+        node->embedding = items[0].first;
+        node->idx = items[0].second;
+        node->left = nullptr;
+        node->right = nullptr;
+        return node;
+    }
+
+    size_t axis = depth % Embedding_T<T>::Dim();
+
+    std::sort(items.begin(), items.end(), 
+        [axis](const std::pair<T, int>& a, const std::pair<T, int>& b) {
+            float coord_a = getCoordinate(a.first, axis);
+            float coord_b = getCoordinate(b.first, axis);
+
+            if (coord_a != coord_b) {
+                return coord_a < coord_b;
+            }
+
+            for (size_t i = 1; i < Embedding_T<T>::Dim(); ++i) {
+                size_t next_axis = (axis + i) % Embedding_T<T>::Dim();
+                float next_a = getCoordinate(a.first, next_axis);
+                float next_b = getCoordinate(b.first, next_axis);
+                
+                if (next_a != next_b) {
+                    return next_a < next_b;
+                }
+            }
+            
+            return a.second < b.second;
+        });
+    
+    int medianIdx = (items.size() - 1) / 2;
+
+    Node<T>* node = new Node<T>();
+    node->embedding = items[medianIdx].first;
+    node->idx = items[medianIdx].second;
+
+    std::vector<std::pair<T, int>> leftItems(items.begin(), items.begin() + medianIdx);
+    std::vector<std::pair<T, int>> rightItems(items.begin() + medianIdx + 1, items.end());
+    
+    node->left = buildKD(leftItems, depth + 1);
+    node->right = buildKD(rightItems, depth + 1);
+    
+    return node;
 }
 
 template <typename T>
@@ -156,5 +205,61 @@ void knnSearch(Node<T> *node,
     You should recursively traverse the tree and maintain a max-heap of the K closest points found so far.
     For now, this is a stub that does nothing.
     */
-    return;
+
+    if (node == nullptr) {
+        return;
+    }
+
+    size_t axis = depth % Embedding_T<T>::Dim();
+
+    float queryCoord = getCoordinate(Node<T>::queryEmbedding, axis);
+    float nodeCoord = getCoordinate(node->embedding, axis);
+
+    Node<T>* nearSubtree;
+    Node<T>* farSubtree;
+    
+    if (queryCoord < nodeCoord) {
+        nearSubtree = node->left; 
+        farSubtree = node->right;
+    } else {
+        nearSubtree = node->right;
+        farSubtree = node->left;
+    }
+
+    knnSearch(nearSubtree, depth + 1, K, heap);
+
+    float distance = Embedding_T<T>::distance(Node<T>::queryEmbedding, node->embedding);
+    
+    int currentHeapSize = static_cast<int>(heap.size());
+    
+    if (currentHeapSize < K) {
+        heap.push(std::make_pair(distance, node->idx));
+    } else {
+        float worstDistanceSoFar = heap.top().first;
+        
+        if (distance < worstDistanceSoFar) {
+            heap.pop();
+            heap.push(std::make_pair(distance, node->idx));
+        }
+    }
+
+    bool shouldSearchFar = false;
+
+    int updatedHeapSize = static_cast<int>(heap.size());
+    
+    if (updatedHeapSize < K) {
+        shouldSearchFar = true;
+    } else {
+
+        float hyperplaneDistance = std::abs(queryCoord - nodeCoord);
+        float currentWorstDistance = heap.top().first;
+        
+        if (hyperplaneDistance < currentWorstDistance) {
+            shouldSearchFar = true;
+        }
+    }
+    
+    if (shouldSearchFar) {
+        knnSearch(farSubtree, depth + 1, K, heap);
+    }
 }
